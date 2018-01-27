1849ec918d2af2cdb3e8b04abf583b0ffcf1f997
Yao Chen <yaochen_nk@hotmail.com>
2018-01-27 15:07:13 +0800

    First commit to create convAcc improvement repo.

---
+++ inference_net/acc_instance.h
@@ -0,0 +1,169 @@
+#ifndef _ACC_INSTANCE_H_
+#define _ACC_INSTANCE_H_
+
+#include "conv_acc_innerpp.h"
+#include "max_pool_acc_innerpp.h"
+#include "config.h"
+
+conv_acc<data_type, data_type_w, data_type_o, Tm, Tn, Tr, Tc, S_max, K_max, IBUF_t, WBUF_t, OBUF_t> convAcc1;
+
+#if _LAYER_MODE_
+void conv_layer_new(
+   int N,
+   int K,
+   int M,
+   int R_IN,
+   int C_IN,
+   int C_OUT,
+   int R_OUT,
+   int S,
+   int P,
+   bool act,
+   data_type_w *layer_weights,
+   data_type_w *layer_bias,
+   int weight_offset,
+   int bias_offset,
+   int in_offset,
+   int out_offset,
+   data_type *in_data_1,
+   data_type_o *out_data_1) {
+
+   convAcc1.conv_layer_acc(N, K, M, R_IN, C_IN, C_OUT, R_OUT, S, P, act, layer_weights, layer_bias, weight_offset, bias_offset, in_offset, out_offset, in_data_1, out_data_1);
+
+}
+#endif
+
+max_pool_acc<data_type, data_type_w, data_type_o, 32, 16, 16, 2, 2> maxPoolAcc1;
+
+#if _LAYER_MODE_
+void max_pool_layer_new(
+        int R_in,
+        int C_in,
+        int N,
+        int K,
+        int R,
+        int C,
+        int S,
+        int P,
+        bool act,
+        data_type *in_data_1,
+        data_type_o *out_data_1) {
+
+    maxPoolAcc1.max_pool_layer_acc(R_in, C_in, N, K, R, C, S, P, act, in_data_1, out_data_1);
+
+}
+#endif
+/*
+void conv_core_syn(data_type_w in_buf_0[8][128][128],
+                   data_type_w w_buf_0[8][32][16][16],
+                   data_type_w b_buf_0[32],
+                   data_type_w out_buf_0[32][32][32],
+                   int param_in[16])
+{
+#pragma HLS INTERFACE s_axilite port=return bundle=CRTL_BUS
+
+#pragma HLS ARRAY_PARTITION variable=in_buf_0  complete dim=1
+#pragma HLS ARRAY_PARTITION variable=w_buf_0   complete dim=1
+#pragma HLS ARRAY_PARTITION variable=w_buf_0   complete dim=2
+#pragma HLS ARRAY_PARTITION variable=out_buf_0 complete dim=1
+
+#pragma HLS RESOURCE variable=in_buf_0  core=RAM_1P_BRAM
+#pragma HLS RESOURCE variable=w_buf_0   core=RAM_1P_BRAM
+#pragma HLS RESOURCE variable=out_buf_0 core=RAM_1P_BRAM
+#pragma HLS RESOURCE variable=param_in  core=RAM_1P_BRAM
+
+#pragma HLS INTERFACE bram port=in_buf_0
+#pragma HLS INTERFACE bram port=w_buf_0
+#pragma HLS INTERFACE bram port=b_buf_0
+#pragma HLS INTERFACE bram port=out_buf_0
+#pragma HLS INTERFACE bram port=param_in
+
+    int param[16];
+    data_type_w b_buf_0_tmp[32];
+#pragma HLS ARRAY_PARTITION variable=b_buf_0_tmp complete
+
+    for(int i = 0; i<16; i++){
+#pragma HLS PIPELINE
+        param[i] = param_in[i];
+    }
+
+    for(int i = 0; i<32; i++){
+        b_buf_0_tmp[i] = b_buf_0[i];
+    }
+
+    convAcc1.conv_core_acc(in_buf_0, w_buf_0, b_buf_0_tmp, out_buf_0, param);
+}
+*/
+
+void conv_core_syn(data_type_w in_buf_0[Tn][IBUF_t][IBUF_t],
+                   data_type_w w_buf_0[Tn][Tm][WBUF_t][WBUF_t],
+                   data_type_w b_buf_0[Tm],
+                   data_type_w out_buf_0[Tm][OBUF_t][OBUF_t],
+                   int param_in[16],
+                   int r_offset,
+                   int c_offset ) {
+
+#pragma HLS INTERFACE s_axilite port=return bundle=CRTL_BUS
+
+#pragma HLS ARRAY_PARTITION variable=in_buf_0  complete dim=1
+#pragma HLS ARRAY_PARTITION variable=w_buf_0   complete dim=1
+#pragma HLS ARRAY_PARTITION variable=w_buf_0   complete dim=2
+#pragma HLS ARRAY_PARTITION variable=out_buf_0 complete dim=1
+
+#pragma HLS RESOURCE variable=in_buf_0  core=RAM_1P_BRAM
+#pragma HLS RESOURCE variable=w_buf_0   core=RAM_1P_BRAM
+#pragma HLS RESOURCE variable=out_buf_0 core=RAM_1P_BRAM
+#pragma HLS RESOURCE variable=param_in  core=RAM_1P_BRAM
+
+#pragma HLS INTERFACE bram port=in_buf_0
+#pragma HLS INTERFACE bram port=w_buf_0
+#pragma HLS INTERFACE bram port=b_buf_0
+#pragma HLS INTERFACE bram port=out_buf_0
+#pragma HLS INTERFACE bram port=param_in
+
+    int param[16];
+    data_type_w b_buf_0_tmp[Tm];
+#pragma HLS ARRAY_PARTITION variable=b_buf_0_tmp complete
+
+    for(int i = 0; i<16; i++){
+#pragma HLS PIPELINE
+        param[i] = param_in[i];
+    }
+
+    for(int i = 0; i<32; i++){
+        b_buf_0_tmp[i] = b_buf_0[i];
+    }
+
+    convAcc1.conv_core_acc(in_buf_0, w_buf_0, b_buf_0_tmp, out_buf_0, param, r_offset, c_offset );
+}
+
+
+/*
+void pool_core_syn(data_type_w in_buf[32][16][16],
+                   data_type_w out_buf[32][16][16],
+                   int pool_param_in[16])
+{
+#pragma HLS INTERFACE s_axilite port=return bundle=CRTL_BUS
+
+#pragma HLS ARRAY_PARTITION variable=in_buf  complete dim=1
+#pragma HLS ARRAY_PARTITION variable=out_buf complete dim=1
+
+#pragma HLS RESOURCE variable=in_buf  core=RAM_1P_BRAM
+#pragma HLS RESOURCE variable=out_buf core=RAM_1P_BRAM
+#pragma HLS RESOURCE variable=pool_param_in  core=RAM_1P_BRAM
+
+#pragma HLS INTERFACE bram port=in_buf
+#pragma HLS INTERFACE bram port=out_buf
+#pragma HLS INTERFACE bram port=pool_param_in
+
+    int param[16];
+    for(int i = 0; i<16; i++) {
+#pragma HLS PIPELINE
+        param[i] = pool_param_in[i];
+    }
+    maxPoolAcc1.mpool_core_acc(in_buf, out_buf, param);
+
+}
+*/
+
+#endif
---
+++ inference_net/construct_net.h
@@ -0,0 +1,184 @@
+
+#ifndef _CONSTRUCT_NET_H_
+#define _CONSTRUCT_NET_H_
+#include <iostream>
+#include <ap_fixed.h>
+#include "config.h"
+#include "acc_instance.h"
+
+using namespace std;
+
+
+void conv_pool_layer(
+        int layer_param[16],
+        int conv_param[16],
+        int pool_param[16],
+        data_type_w *conv_weight_mem_port,
+        data_type_w *conv_bias_mem_port,
+        data_type_w *temp_out_0_1,
+        data_type_w *temp_out_1_1,
+        data_type_w in_buf_0[][IBUF_t][IBUF_t],
+        data_type_w w_buf_0[][Tm][WBUF_t][WBUF_t],
+        data_type_w b_buf_0[],
+        data_type_w out_buf_0[][OBUF_t][OBUF_t]){
+
+    int acc_call_rounds  = 0;
+    for(int r = 0; r < layer_param[5]&&r<IBUF_t; r += IBUF_t) {
+        for (int c = 0; c < layer_param[6]&&c<IBUF_t; c += IBUF_t) {
+            for (int m = 0; m < layer_param[2]; m += Tm) {
+                for (int n = 0; n < layer_param[0]; n += Tn) {
+
+                    acc_call_rounds++;
+
+                    // fill in buffer
+                    conv_param[1] = n;
+                    conv_param[2] = r;
+                    conv_param[3] = c;
+
+                    convAcc1.b_buf_load(b_buf_0, conv_bias_mem_port, 0, m);
+                    convAcc1.w_buf_load(w_buf_0, conv_weight_mem_port, 0, n, m, layer_param[1], layer_param[0], layer_param[2]);
+                    convAcc1.in_buf_load(in_buf_0, temp_out_0_1, 0, n, r, c, layer_param[7], layer_param[1], layer_param[8], layer_param[3], layer_param[4], layer_param[7]);
+
+                    ofstream conv_out;
+                    conv_out.open("in_buf_data.txt", ios::app);
+                    conv_out <<"conv input: "<< endl;
+                    for (int i = 0; i < layer_param[0]; i++) {
+                        for (int j = 0; j < layer_param[5]+layer_param[8]*2; j++) {
+                            for(int k = 0; k < layer_param[6]+layer_param[8]*2; k++){
+                                conv_out << in_buf_0[i][j][k] << " ";
+                            }
+                            conv_out << endl;
+                        }
+                        conv_out << endl;
+                    }
+                    conv_out.close();
+
+#if _C_DEBUG_MODE_
+                    cout << "conv acc round : "  << acc_call_rounds << endl;
+                    cout << "b buf 0 data :" << endl;
+                    for (int i =0; i<Tm && i<layer_param[2]; i++) {
+                        cout << b_buf_0[i] << "  ";
+                    }
+                    cout << endl;
+                    cout << endl;
+                    cout<< "w buf 0 data :" << endl;
+                    for(int a=0; a<Tn && a<layer_param[0]; a++) {
+                        for (int i = 0; i < Tm && i < layer_param[2]; i++) {
+                            for (int j = 0; j < layer_param[1]; j++) {
+                                for (int k = 0; k < layer_param[1]; k++) {
+                                    cout << w_buf_0[a][i][j][k] << "  ";
+                                }
+                                cout << endl;
+                            }
+                            cout << endl;
+                        }
+                        cout << endl;
+                    }
+                    cout << endl;
+                    cout << endl;
+                    cout << "in buf 0 data :" << endl;
+                    for (int i =0; i<layer_param[0] && i<Tn; i++){
+                        for (int j=0; j<layer_param[3] && j<IBUF_t; j++){
+                            for(int k=0; k<layer_param[4] && k<IBUF_t; k++) {
+                                cout << in_buf_0[i][j][k] << " ";
+                            }
+                            cout << endl;
+                        }
+                        cout << endl;
+                    }
+                    cout << endl;
+                    cout << endl;
+#endif
+
+                    // compute buffered data
+                    for(int r_offset=0; r_offset < (OBUF_t>layer_param[5]?layer_param[5]:OBUF_t); r_offset+=Tr) {
+                        for(int c_offset=0; c_offset < (IBUF_t>layer_param[5]?layer_param[5]:IBUF_t);c_offset+=Tc) {
+                            conv_core_syn(in_buf_0, w_buf_0, b_buf_0, out_buf_0, conv_param, r_offset, c_offset);
+                        }
+                    }
+                    //pool_core_syn(out_buf_0, out_buf_1, pool_engine_param_in);
+
+                    // read results out
+                    convAcc1.output_res(out_buf_0, temp_out_1_1, 0, n, m, r, c, layer_param[0], layer_param[2], layer_param[5], layer_param[6], 1);
+                    cout << "out buf 0 data :" << endl;
+                    conv_out.open("conv_buf_data.txt", ios::app);
+                    conv_out <<"conv output: "<< endl;
+                    for (int i = 0; i < layer_param[2]; i++) {
+                        for (int j = 0; j < layer_param[5]; j++) {
+                            for(int k = 0; k < layer_param[6]; k++){
+                                conv_out << *(temp_out_1_1+i*layer_param[5]*layer_param[6]+j*layer_param[6]+k) << " ";
+                            }
+                            conv_out << endl;
+                        }
+                        conv_out << endl;
+                    }
+                    conv_out.close();
+                }
+            }
+        }
+    }
+
+    cout << "acc call round = " << acc_call_rounds << endl;
+}
+
+void   inference_net(
+   data_type_w *conv_weight_port,
+   data_type_w *fc_weight_port,
+   data_type_w *fc_bias_port,
+   data_type_o   fc_3_out_a[10*1*1],
+   data_type_o   temp_out_0_1[4704],
+   data_type_o   temp_out_1_1[4704]){
+
+#if _HLS_MODE_
+#pragma HLS INTERFACE s_axilite port=return bundle=CRTL_BUS
+#pragma HLS INTERFACE m_axi depth=2550 port=conv_weight_port
+#pragma HLS INTERFACE m_axi depth=4000 port=temp_out_0_1
+#pragma HLS INTERFACE m_axi depth=10 port=temp_out_1_1
+#pragma HLS INTERFACE m_axi depth=10 port=fc_weight_port
+#pragma HLS INTERFACE m_axi depth=4704 port=fc_bias_port
+#pragma HLS INTERFACE m_axi depth=4704 port=fc_3_out_a
+#endif
+
+#if _C_DEBUG_MODE_
+#if _KERNEL_DEBUG_
+   cout << "starting forward network process..........................." << endl;
+   cout << "..........................................................." << endl;
+#endif
+#endif
+
+#if _LAYER_MODE_
+
+   int shift_weight_conv1_1 = 0;
+
+   int shift_weight_conv2_1 = 150;
+
+   int shift_weight_fc1 = 0;
+   int shift_bias_fc1 = 0;
+
+
+   conv_layer_new(1, 5, 6, 28, 28, 28, 28, 1, 2, 1, conv_weight_port, shift_weight_conv1_1, 0, 0,  temp_out_0_1,  temp_out_1_1);
+
+   max_pool_layer_new(28, 28, 6, 2, 14, 14, 2, 0, 1,  temp_out_1_1,  temp_out_0_1);
+
+   conv_layer_new(6, 5, 16, 14, 14, 10, 10, 1, 0, 1, conv_weight_port, shift_weight_conv2_1, 0, 0,  temp_out_0_1,  temp_out_1_1);
+
+   max_pool_layer_new(10, 10, 16, 2, 5, 5, 2, 0, 1,  temp_out_1_1,  temp_out_0_1);
+
+   conv_layer_new(16, 5, 10, 5, 5, 1, 1, 5, 0, 1, fc_weight_port, fc_bias_port, shift_weight_fc1, shift_bias_fc1, 0, 0,  temp_out_0_1,  temp_out_1_1);
+
+   for (int i = 0; i < 10; i++) {
+      fc_3_out_a[i+0] = temp_out_1_1[i/1];
+      
+   }
+
+#endif
+
+#if _C_DEBUG_MODE_
+#if _KERNEL_DEBUG_
+   cout << "Finished forward network process .........................." << endl;
+   cout << "..........................................................." << endl;
+#endif
+#endif
+}
+
+#endif //_CONSTRUCT_NET_H_
